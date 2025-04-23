0efa5708b1e5bb0234508d8a9bb1e2ccf71ccbc5
Pedro Tepedino <pedrotepedinossauro@gmail.com>
2025-04-23 14:28:21 -0300

    Exercicio Resolvido

---
+++ meu_modulo.py
@@ -0,0 +1,11 @@
+def soma(a, b):
+    return a + b
+
+def squared(x):
+  return x ** 2
+
+def cumprimentar(nome):
+    print(f"Olá, {nome}! Seja bem-vindo.")
+
+def cumprimentar2(cumprimento, nome):
+    print(cumprimento + ", " + nome + "!")
